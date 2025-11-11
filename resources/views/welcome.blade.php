8879c8e4cc249e706670225b4b9c343c074df787
Javier Martinez <javimr1224@gmail.com>
2025-11-12 00:15:29 +0100

    cambios menores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,8 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="antialiased bg-gray-900 text-white font-sans">
-    <div class="relative min-h-screen flex flex-col items-center justify-center">
+<body class="antialiased bg-gray-900 text-white font-sans flex flex-col min-h-screen">
+    <div class="flex-grow">
         <header
             style="width: 100%; background: #020617; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.75); font-family: 'Press Start 2P', cursive;">
             <div class="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-6">
@@ -60,47 +60,42 @@
                         @auth
                             <a href="{{ url('/dashboard') }}" class="text-sm font-semibold text-gray-300 hover:text-white">Dashboard</a>
                         @else
-                            <a href="{{ route('login') }}" class="ml-8 text-gray-300 hover:text-white">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-label="Log in">
-                                    <title>Log in</title>
+                                    <title>Search</title>
                                     <path
                                         d="M17.2116 14.8272V0H0V16.8627H14.6611L21.7184 24L24 21.6925L17.2116 14.8272ZM13.9844 13.599H3.22718V3.26375H13.9844V13.599ZM6.99222 12.511H4.30291V9.79126H6.99222V12.511Z"
                                         fill="white" />
                                 </svg>
                             </a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-8 text-gray-300 hover:text-white">
                                     <svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-                                        aria-label="Register">
+                                        aria-label="Register" class="ml-8">
                                         <title>Modo oscuro</title>
                                         <path
                                             d="M10.9316 23.8333C17.3398 23.8333 22.68 18.474 22.68 11.9167C22.68 5.35935 17.3398 0 10.806 0C9.61229 0 8.41861 0.189154 7.22492 0.567461C6.65949 0.756615 6.28254 1.32408 6.28254 1.95459C6.34537 2.5851 6.78514 3.08951 7.4134 3.15256C10.8688 3.65697 13.4446 6.62037 13.4446 10.0882C13.4446 13.9343 10.3034 17.0869 6.47102 17.0869C4.90038 17.0869 3.39257 16.5825 2.13606 15.5736C1.63345 15.1953 0.942373 15.1953 0.502596 15.5736C-9.53674e-06 15.9519 -0.12566 16.6455 0.125643 17.213C2.26171 21.2482 6.34537 23.8333 10.9316 23.8333ZM12.942 2.90035C17.0257 3.84612 20.0413 7.56614 20.0413 11.9167C20.0413 17.0238 15.8948 21.1852 10.806 21.1852C8.98404 21.1852 7.22492 20.6808 5.77994 19.672C6.03124 19.672 6.28254 19.672 6.53384 19.672C11.8112 19.672 16.1461 15.3214 16.1461 10.0251C16.2718 7.25088 15.0153 4.66579 12.942 2.90035Z"
                                             fill="white" />
                                     </svg>
-                                </a>
-                                <a href="{{ route('login') }}" class="ml-8 relative">
+                                <a href="{{ route('login') }}" class="ml-12 relative">
                                     <img src="{{ asset('images/button.png') }}" alt="Login button" style="width: 100px;">
                                     <span class="absolute left-1/2 -translate-x-1/2 -translate-y-1/2"
                                         style="font-family: 'Press Start 2P', cursive; text-shadow: 2px 2px 4px #000000; font-size: 10px; top: 45%;">Login</span>
                                 </a>
-                            @endif
                         @endauth
                     @endif
                 </div>
             </div>
         </header>
-        <div class="relative w-full h-96">
+        <div class="relative w-full">
             <img class="w-full h-auto gap-8"
                 src="{{ asset('videos/may-sitting-near-waterfall-pokemon-emerald-pixel-wallpaperwaifu-com-ezgif.com-video-to-gif-converter.gif') }}"
                 alt="Microgames retro background">
-            <div class="absolute inset-0 flex flex-col items-center justify-center gap-4 animate-float">
+            <div class="absolute inset-0 flex flex-col items-center justify-start m-20  gap-16 animate-float">
                 <h4 class="text-stroke" style="font-family: 'Press Start 2P', cursive; font-size: 30px;">Play now!</h4>
                 <img class="imagen-principal w-96" src="{{ asset('images/retro-games.png') }}" alt="Retro Games">
             </div>
+            <div class="wave-effect"></div>
         </div>
 
-        <footer class="w-full bg-[#020617] text-white py-12 relative overflow-hidden" style="font-family: 'Helvetica Neue', sans-serif;">
+    <footer class="w-full bg-[#020617] text-white py-12 relative overflow-hidden" style="font-family: 'Helvetica Neue', sans-serif;">
             <div class="absolute top-0 left-0 right-0 h-px bg-white opacity-20"></div>
 
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -154,9 +149,7 @@
                             <img alt="Spotify" class="w-full h-full object-cover" src="{{ asset('images/spoti.png') }}" />
                         </a>
                         <a href="#" class="w-10 h-10 hover:opacity-80 transition-opacity">
-                            <div class="relative w-full h-full overflow-hidden">
-                                <img alt="YouTube" class="absolute h-[170.47%] left-[-2.63%] max-w-none top-[-35.23%] w-[105.26%]" src="{{ asset('images/yt.png') }}" />
-                            </div>
+                            <img alt="YouTube" class="w-full h-full object-cover" src="{{ asset('images/yt.png') }}" />
                         </a>
                         <a href="#" class="w-10 h-10 hover:opacity-80 transition-opacity">
                             <img alt="TikTok" class="w-full h-full object-cover" src="{{ asset('images/tiktok.png') }}" />
@@ -171,7 +164,6 @@
             </div>
         </footer>
 
-    </div>
 </body>
 
 </html>
