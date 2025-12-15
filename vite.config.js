3148767358ac903372f9eb200d5d5cfaf2acfe9c
Javier Martinez <javier.martinez@bluumi.com>
2025-12-15 10:00:59 +0100

    cambio de vite produccion

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,40 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: !isProduction, // solo HMR en desarrollo
         }),
     ],
-    server: {
-        host: true,               
-        port: 5173,     
-        strictPort: true,     
-        hmr: {
-            host: 'localhost',
-            port: 5173,
-        },
-        watch: {
-            usePolling: true,
-            interval: 100,
-            ignored: [
-                '**/docker/**',
-                '**/storage/**',
-                '**/vendor/**',
-                '**/node_modules/**',
-            ],
-        },
-    },
+    build: isProduction
+        ? {
+              manifest: true,
+              outDir: 'public/build',
+              rollupOptions: {
+                  input: 'resources/js/app.js',
+              },
+          }
+        : undefined,
+    server: !isProduction
+        ? {
+              host: true,
+              port: 5173,
+              strictPort: true,
+              hmr: { host: 'localhost', port: 5173 },
+              watch: {
+                  usePolling: true,
+                  interval: 100,
+                  ignored: [
+                      '**/docker/**',
+                      '**/storage/**',
+                      '**/vendor/**',
+                      '**/node_modules/**',
+                  ],
+              },
+          }
+        : undefined,
 });
