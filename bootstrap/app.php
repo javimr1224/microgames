2ec28fe661f5ba791341fe5e9a43e3767dbca54c
Javier Martinez <javier.martinez@bluumi.com>
2025-12-16 13:51:32 +0100

    error login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-        
+    ->withMiddleware(function (Middleware $middleware): void {        
         $middleware->alias([
             'game.visits' => TrackGameVisits::class,
         ]);
