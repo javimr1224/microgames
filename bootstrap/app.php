b3cd20226b52b7f0828f44499c7068a336b146c4
Javier Martinez <javier.martinez@bluumi.com>
2025-12-16 14:08:25 +0100

    proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {        
+        $middleware->trustProxies(at: '*'); 
+        
+        $middleware->statefulApi();
+
         $middleware->alias([
             'game.visits' => TrackGameVisits::class,
         ]);
